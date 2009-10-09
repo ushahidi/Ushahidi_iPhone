9bc83f20e788344acb9e526ae319d743ff98ca0d
Wilfred Mworia <wmworia@wilfred-mworias-macbook.local>
2009-10-09 22:45:05 +0300

    initial commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ushahidi
+//
+//  Created by Wilfred Mworia on 9/19/09.
+//  Copyright African Pixel 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}

effc3185b8ea29f48aa282cac15893b455893101
David Kobia <david@ushahidi.com>
2010-05-05 09:24:02 -0400

    * Added LGPL license

--- API.m
+++ API.m
@@ -1,10 +1,22 @@
-//
-//  API.m
-//  Ushahidi
-//
-//  Created by Wilfred Mworia on 9/21/09.
-//  Copyright 2009 African Pixel. All rights reserved.
-//
+/*****************************************************************************
+ ** Copyright (c) 2010 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
 
 #import "API.h"
 #import "UshahidiProjAppDelegate.h"

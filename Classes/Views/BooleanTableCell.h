8c9e63fc0cb67fac1a6e1794f300f1b594dac42b
Dale Zak <dalezak@gmail.com>
2010-08-23 09:44:52 -0600

    Updated license description

--- Classes/Views/BooleanTableCell.h
+++ Classes/Views/BooleanTableCell.h
@@ -1,10 +1,22 @@
-//
-//  BooleanCell.h
-//  gTasks
-//
-//  Created by Dale Zak on 10-08-03.
-//  Copyright 2010 Ushahidi. All rights reserved.
-//
+/*****************************************************************************
+ ** Copyright (c) 2010 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
 
 #import <UIKit/UIKit.h>
 
--- Classes/iPhone/AppDelegate_iPhone.m
+++ Classes/iPhone/AppDelegate_iPhone.m
@@ -1,10 +1,22 @@
-//
-//  AppDelegate_iPhone.m
-//  Ushahidi_iPhone
-//
-//  Created by Dale Zak on 10-08-09.
-//  Copyright Ushahidi 2010. All rights reserved.
-//
+/*****************************************************************************
+ ** Copyright (c) 2010 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
 
 #import "AppDelegate_iPhone.h"
 
